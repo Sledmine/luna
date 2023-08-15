fe3eea6e5f4108bb9e3ee97ed91a03b0f5495773
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-08-15 00:16:10 -0600

    improve table.filter, use numbers in string.tohex

--- test.lua
+++ test.lua
@@ -61,6 +61,11 @@ local filtered = table.filter(t, function(v, k)
 end)
 assert(filtered[1] == "a")
 assert(filtered[3] == "c")
+filtered = table.filter(t, function(v, k)
+    return v ~= "b"
+end, true)
+assert(filtered[1] == "a")
+assert(filtered[2] == "c")
 
 local mapped = table.map(t, function(v, k)
     return v .. "!"
